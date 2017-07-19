f05834f16f821651fccbec21399809c0c92cbdf2
lorris saint-genez <lorris.saint-genez@epita.fr>
2017-07-19 11:44:51 +0200

    [About] Done

--- src/about/about.js
+++ src/about/about.js
@@ -9,13 +9,9 @@ class About extends Component {
         <div className="container">
           <div className="row">
             <div className="col-xs-12">
-              <div className="col-xs-12 col-lg-6 col-md-6">
-                <img src={Lorris} className="img-circle profile-picture" />
-              </div>
-              <div className="col-xs-12 col-lg-6 col-md-6">
-                <div className="hidiho">Hi, I'm <b>Lorris Saint-Genez</b> !</div>
-                <small>Engineer student <b>@EPITA</b>, Full-Stack Web Developer</small>
-              </div>
+              <img src={Lorris} className="img-circle profile-picture" />
+              <div className="hidiho">Hi, I'm <b>Lorris Saint-Genez</b> !</div>
+              <small>Engineer student <b><a href="http://www.epita.fr/"> @EPITA</a></b>, Full-Stack Web Developer</small>
             </div>
           </div>
         </div>
