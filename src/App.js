27bb9b0359c792b404e3027ae3a2b64db446b51d
lorris saint-genez <lorris.saint-genez@epita.fr>
2017-07-22 18:52:54 +0200

    [Footer] Anchor OK - except EI/Edge

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import ScrollableAnchor from 'react-scrollable-anchor'
 
+import Footer from './footer/footer.js';
 import About from './about/about.js';
 import Education from './education/education.js';
 import Experience from './experience/experience.js';
@@ -14,20 +16,32 @@ import './App.css';
 class App extends Component {
 
   render() {
+
     return (
       <div className="App">
         <div className="background-image-style" />
         <a href="https://www.linkedin.com/in/lorris-saint-genez/">
-          <img alt="linkedin" src={Linkedin} className="linkedin"  />
+          <img alt="linkedin" src={Linkedin} className="linkedin" />
         </a>
         <a href="https://github.com/LorrisSaintGenez">
           <img alt="github" src={Github} className="github "/>
         </a>
-        <About/>
-        <Education/>
-        <Experience/>
-        <Skill/>
-        <Contact/>
+        <Footer />
+        <ScrollableAnchor id={'about-anchor'}>
+          <About/>
+        </ScrollableAnchor>
+        <ScrollableAnchor id={'education-anchor'}>
+          <Education/>
+        </ScrollableAnchor>
+        <ScrollableAnchor id={'experience-anchor'}>
+          <Experience/>
+        </ScrollableAnchor>
+        <ScrollableAnchor id={'skill-anchor'}>
+          <Skill/>
+        </ScrollableAnchor>
+        <ScrollableAnchor id={'contact-anchor'}>
+          <Contact/>
+        </ScrollableAnchor>
       </div>
     );
   }
