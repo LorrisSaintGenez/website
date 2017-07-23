39e96bb4ac01373087c82116297e8e3eb3c72de9
lorris saint-genez <lorris.saint-genez@epita.fr>
2017-07-23 23:01:17 +0200

    [Skill] Use of anchor + redesign

--- src/skill/skill.js
+++ src/skill/skill.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
-import ScrollableAnchor from 'react-scrollable-anchor'
+import ScrollableAnchor, { goToAnchor } from 'react-scrollable-anchor'
 
-import _ from 'lodash';
-
-import CategoryItem from '../common/categoryItem.js';
 import SkillComponent from './skill-component.js';
 
 class Skill extends Component {
@@ -12,123 +9,113 @@ class Skill extends Component {
     super(props);
 
     this.state = {
-      skillset: [
+      languages: [
         {
-          languages: [
-            {
-              name: "Bootstrap",
-              image: "devicon-bootstrap-plain colored"
-            },
-            {
-              name: "C#",
-              image: "devicon-csharp-plain colored"
-            },
-            {
-              name: "C++",
-              image: "devicon-cplusplus-plain colored"
-            },
-            {
-              name: "CSS3",
-              image: "devicon-css3-plain colored"
-            },
-            {
-              name: "Go",
-              image: "devicon-go-plain"
-            },
-            {
-              name: "HTML5",
-              image: "devicon-html5-plain colored"
-            },
-            {
-              name: "Java",
-              image: "devicon-java-plain colored"
-            },
-            {
-              name: "JavaScript",
-              image: "devicon-javascript-plain colored"
-            },
-            {
-              name: "Laravel",
-              image: "devicon-laravel-plain colored"
-            },
-            {
-              name: ".Net",
-              image: "devicon-dot-net-plain colored"
-            },
-            {
-              name: "Node JS",
-              image: "devicon-nodejs-plain colored"
-            },
-            {
-              name: "PHP",
-              image: "devicon-php-plain colored"
-            },
-            {
-              name: "Python",
-              image: "devicon-python-plain colored"
-            },
-            {
-              name: "React JS",
-              image: "devicon-react-original colored"
-            },
-          ],
+          name: "Bootstrap",
+          image: "devicon-bootstrap-plain colored"
         },
         {
-          databases: [
-            {
-              name: "MongoDB",
-              image: "devicon-mongodb-plain colored"
-            },
-            {
-              name: "MySQL",
-              image: "devicon-mysql-plain colored"
-            },
-            {
-              name: "PostgreSQL",
-              image: "devicon-postgresql-plain colored"
-            },
-          ],
+          name: "C#",
+          image: "devicon-csharp-plain colored"
         },
         {
-          tools: [
-            {
-              name: "Docker",
-              image: "devicon-docker-plain colored"
-            },
-            {
-              name: "Git",
-              image: "devicon-git-plain colored"
-            },
-            {
-              name: "Heroku",
-              image: "devicon-heroku-original colored"
-            },
-            {
-              name: "Slack",
-              image: "devicon-slack-plain"
-            },
-            {
-              name: "Trello",
-              image: "devicon-trello-plain colored"
-            },
-          ],
+          name: "C++",
+          image: "devicon-cplusplus-plain colored"
         },
         {
-          platforms: [
-            {
-              name: "Android",
-              image: "devicon-android-plain colored"
-            },
-            {
-              name: "Ubuntu",
-              image: "devicon-ubuntu-plain colored"
-            },
-            {
-              name: "Windows",
-              image: "devicon-windows8-original colored"
-            },
-          ],
-        }
+          name: "CSS3",
+          image: "devicon-css3-plain colored"
+        },
+        {
+          name: "Go",
+          image: "devicon-go-plain"
+        },
+        {
+          name: "HTML5",
+          image: "devicon-html5-plain colored"
+        },
+        {
+          name: "Java",
+          image: "devicon-java-plain colored"
+        },
+        {
+          name: "JavaScript",
+          image: "devicon-javascript-plain colored"
+        },
+        {
+          name: "Laravel",
+          image: "devicon-laravel-plain colored"
+        },
+        {
+          name: ".Net",
+          image: "devicon-dot-net-plain colored"
+        },
+        {
+          name: "Node JS",
+          image: "devicon-nodejs-plain colored"
+        },
+        {
+          name: "PHP",
+          image: "devicon-php-plain colored"
+        },
+        {
+          name: "Python",
+          image: "devicon-python-plain colored"
+        },
+        {
+          name: "React JS",
+          image: "devicon-react-original colored"
+        },
+      ],
+      databases: [
+        {
+          name: "MongoDB",
+          image: "devicon-mongodb-plain colored"
+        },
+        {
+          name: "MySQL",
+          image: "devicon-mysql-plain colored"
+        },
+        {
+          name: "PostgreSQL",
+          image: "devicon-postgresql-plain colored"
+        },
+      ],
+      tools: [
+        {
+          name: "Docker",
+          image: "devicon-docker-plain colored"
+        },
+        {
+          name: "Git",
+          image: "devicon-git-plain colored"
+        },
+        {
+          name: "Heroku",
+          image: "devicon-heroku-original colored"
+        },
+        {
+          name: "Slack",
+          image: "devicon-slack-plain"
+        },
+        {
+          name: "Trello",
+          image: "devicon-trello-plain colored"
+        },
+      ],
+      platforms: [
+        {
+          name: "Android",
+          image: "devicon-android-plain colored"
+        },
+        {
+          name: "Ubuntu",
+          image: "devicon-ubuntu-plain colored"
+        },
+        {
+          name: "Windows",
+          image: "devicon-windows8-original colored"
+        },
       ],
       types: [
         {
@@ -144,14 +131,39 @@ class Skill extends Component {
           name: "Platforms"
         },
       ],
-      description_id: 0
+      description_id: -1
     };
 
     this.descriptionDisplay = this.descriptionDisplay.bind(this);
   }
 
   descriptionDisplay(id) {
-    this.setState({description_id: id});
+
+    if (id === this.state.description_id) {
+      this.setState({description_id: -1});
+      goToAnchor('skill-anchor');
+    }
+    else {
+      this.setState({description_id: id});
+
+      switch (id) {
+        case 0:
+          goToAnchor('languages');
+          break;
+        case 1:
+          goToAnchor('databases');
+          break;
+        case 2:
+          goToAnchor('tools');
+          break;
+        case 3:
+          goToAnchor('platforms');
+          break;
+        default:
+          break;
+      }
+    }
+
   }
 
   render() {
@@ -163,16 +175,43 @@ class Skill extends Component {
             <ScrollableAnchor id={'skill-anchor'}>
               <div>
                 <div id="skills">Skills</div>
-                <div className="col-xs-12 col-lg-8 col-md-8">
-                  {_.map(this.state.types, (type, index) => {
-                    return (<CategoryItem key={index} isOnLeft={true} item={type} index={index} description_id={this.state.description_id} descriptionDisplay={this.descriptionDisplay} />)
-                  })}
-                </div>
-                <div className="col-xs-12 col-lg-4 col-md-4">
-                  {_.map(this.state.skillset, (skilltype, index) => {
-                      return (<SkillComponent skilltype={skilltype} hidden={this.state.description_id !== index} key={index} />)
-                    }
-                  )}
+                <div className="col-xs-12 col-lg-12 col-md-12">
+                  <ScrollableAnchor id={'languages'}>
+                    <SkillComponent
+                      description_id={this.state.description_id}
+                      descriptionDisplay={this.descriptionDisplay}
+                      hidden={this.state.description_id !== 0}
+                      index={0}
+                      skilltype={this.state.languages}
+                      title={this.state.types[0].name} />
+                  </ScrollableAnchor>
+                  <ScrollableAnchor id={'databases'}>
+                    <SkillComponent
+                      description_id={this.state.description_id}
+                      descriptionDisplay={this.descriptionDisplay}
+                      hidden={this.state.description_id !== 1}
+                      index={1}
+                      skilltype={this.state.databases}
+                      title={this.state.types[1].name} />
+                  </ScrollableAnchor>
+                  <ScrollableAnchor id={'tools'}>
+                    <SkillComponent
+                      description_id={this.state.description_id}
+                      descriptionDisplay={this.descriptionDisplay}
+                      hidden={this.state.description_id !== 2}
+                      index={2}
+                      skilltype={this.state.tools}
+                      title={this.state.types[2].name} />
+                  </ScrollableAnchor>
+                  <ScrollableAnchor id={'platforms'}>
+                    <SkillComponent
+                      description_id={this.state.description_id}
+                      descriptionDisplay={this.descriptionDisplay}
+                      hidden={this.state.description_id !== 3}
+                      index={3}
+                      skilltype={this.state.platforms}
+                      title={this.state.types[3].name} />
+                  </ScrollableAnchor>
                 </div>
               </div>
             </ScrollableAnchor>
